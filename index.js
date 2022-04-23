6a42455f39c50c38b4e0a27620c93ed8dc484554
Israel Albarrán <Isra@MacBook-Air-de-MAC.local>
2022-04-23 15:59:43 -0500

    bd conectada exitosamente

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ window.addEventListener('DOMContentLoaded', () =>{
 const Qform = document.getElementById('Qform')
 
 Qform.addEventListener('submit', (e) => {
-    e.preventDefault() 
+    e.preventDefault();
+    
    
-   
-   const Materia = Qform['Materia']
-   const Area = Qform['Area']
-   const Pregunta = Qform['Pregunta']
-   const RespuestaC = Qform['RespuestaC']
-   const Respuesta1= Qform['Respuesta1']
-   const Respuesta2 = Qform['Respuesta2']
-   const Respuesta3 = Qform['Respusta3']
+    const Materia = document.getElementById('Materia')
+    saveQ(Materia.value)
 
-   
-   saveQ(Materia.value,Area.value,Pregunta.value,RespuestaC.value,Respuesta1.value, Respuesta2.value,Respuesta3.value)
+    const Area = document.getElementById('Area')
+    saveQ(Area.value)
 
+    const Pregunta= document.getElementById('Pregunta')
+    saveQ(Pregunta.value)
+
+    const RespuestaC= document.getElementById('RespuestaC')
+    saveQ(RespuestaC.value)
+
+    const Respuesta1 = document.getElementById('Respuesta1')
+    saveQ(Respuesta1.value)
+
+    const Respuesta2 = document.getElementById('Respuesta2')
+    saveQ(Respuesta2.value)
+
+    const Respusta3 = document.getElementById('Respuesta3')
+    saveQ(Respusta3.value)
+   
    Qform.reset()
 
 })
